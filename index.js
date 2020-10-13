acba89243ef48873676e0cb86e3be53ecf334731
Kevin Palermo <kevpalermo@gmail.com>
2020-10-13 19:56:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,19 @@ var newObj = Object.assign({}, obj)
 var obj = { key: value }
 
 function deleteFromObjectByKey(object, key) {
+<<<<<<< HEAD
   delete newObj[key]
+=======
+  delete newObj.key
+>>>>>>> 30ac6e899b0d97f3703a1cfd60972ac37f419059
   return newObj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
+<<<<<<< HEAD
   delete object[key];
+=======
+  delete object.key;
+>>>>>>> 30ac6e899b0d97f3703a1cfd60972ac37f419059
   return object;
 }
