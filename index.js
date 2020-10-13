ba75be01ede70078773f7e08949acccee295c99d
Kevin Palermo <kevpalermo@gmail.com>
2020-10-13 20:05:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function deleteFromObjectByKey(object, key){
+function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
   return object
 }
